eab6976d1dfa73080201265a331e22ee744bfee8
Pedro Ferrer <pedro.g.ferrer@hotmail.com>
2018-07-31 23:45:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,18 +5,25 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+function coinFlip() {
+    return (Math.floor(Math.random() * 2) == 0) ? 'heads!' : 'tails!';
+}; 
+
 client.on('message', msg => {
   switch(msg.content){
   // !ping
   case 'ping':
-    msg.reply('Pong')
-  break;
+    msg.reply('Pong');
+    break;
 
   case 'roll':
-    msg.reply('Your roll was ' +Math.floor((Math.random() * 100) + 1))
-  break;
-  
- 
+    msg.reply('Your roll was' + Math.floor((Math.random() * 100) + 1));
+
+    break;
+
+  case 'flip':
+    msg.reply('You got ' + coinFlip());
+    break;
 }
 });
 
